7098da646b8b3f5b5ba3e019d9dc0846c350bb7e
bcfchen <bcfchen@gmail.com>
2020-03-04 13:56:41 +0900

    create readme. create main application, express server, and dockerize

---
+++ transpiled/App.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _interopRequireDefault = require("/Users/beckchen/Projects/micro-demo/micro-demo-header/node_modules/@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _default = function _default() {
+  return _react.default.createElement("header", null, _react.default.createElement("h1", null, "Logo"), _react.default.createElement("nav", null, _react.default.createElement("ul", null, _react.default.createElement("li", null, "About"), _react.default.createElement("li", null, "Contact"))));
+};
+
+exports.default = _default;
